3c5a26370076717de05b15c530f809b061d29862
saumaIqor <josedavids123@live.com>
2024-06-11 17:06:22 -0500

    aurora v-0.1

---
+++ src/main/java/NameCheap/nameCheap/NameCheapApplication.java
@@ -0,0 +1,13 @@
+package NameCheap.nameCheap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameCheapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameCheapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NameCheap/nameCheap/NameCheapApplicationTests.java
@@ -0,0 +1,13 @@
+package NameCheap.nameCheap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameCheapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
